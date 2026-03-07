db85387f3d9e74de856673ff539e1b5f49492f2e
brensch <brendolini@protonmail.com>
2026-03-07 12:18:52 -0800

    2 ezish

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module leet
 
-go 1.22
+go 1.24.0
+
+require golang.org/x/text v0.34.0
--- problems/02_valid_palindrome/solution.go
+++ problems/02_valid_palindrome/solution.go
@@ -1,6 +1,32 @@
 package valid_palindrome
 
+import (
+	"fmt"
+	"strings"
+)
+
 // IsPalindrome reports whether s is a valid palindrome after normalization.
 func IsPalindrome(s string) bool {
-	return false
+
+	sCleaned := CleanString(s)
+	for i := 0; i < len(sCleaned); i++ {
+		if sCleaned[i] != sCleaned[len(sCleaned)-1-i] {
+			return false
+		}
+	}
+	return true
+}
+
+func CleanString(s string) string {
+	start := 'a'
+	end := 'z'
+	sCleaned := []rune{}
+	s = strings.ToLower(s)
+	for _, char := range s {
+		if char >= start && char <= end {
+			sCleaned = append(sCleaned, char)
+		}
+	}
+	fmt.Println(string(sCleaned))
+	return string(sCleaned)
 }
