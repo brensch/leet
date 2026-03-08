bf2cd18d4b0e49801b89277e01587df874c0f362
brensch <brendolini@protonmail.com>
2026-03-07 17:59:37 -0800

    islands ez

--- problems/08_number_of_islands/solution.go
+++ problems/08_number_of_islands/solution.go
@@ -2,5 +2,33 @@ package number_of_islands
 
 // NumIslands counts connected components of land in the grid.
 func NumIslands(grid [][]byte) int {
-	return 0
+
+	islands := 0
+	// iterate over all the blocks, once we find one, traverse in all directions and remove from unvisited blocks
+	for i := 0; i < len(grid); i++ {
+		for j := 0; j < len(grid[i]); j++ {
+			if grid[i][j] == '1' {
+				// record we found an island
+				islands++
+				// set all attached blocks to 0
+				traverseIsland(grid, i, j)
+			}
+		}
+	}
+
+	return islands
+}
+
+// traverseIsland sets all points connected to a block to 0
+func traverseIsland(grid [][]byte, x, y int) {
+	if x < 0 || x >= len(grid) || y < 0 || y >= len(grid[x]) || grid[x][y] != '1' {
+		return
+	}
+
+	grid[x][y] = '0'
+
+	traverseIsland(grid, x+1, y)
+	traverseIsland(grid, x-1, y)
+	traverseIsland(grid, x, y+1)
+	traverseIsland(grid, x, y-1)
 }
