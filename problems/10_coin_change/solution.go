f0688e3429f067da06aef42c70628456bd8aafdc
brensch <brendolini@protonmail.com>
2026-03-07 19:03:22 -0800

    dp

--- problems/10_coin_change/solution.go
+++ problems/10_coin_change/solution.go
@@ -1,6 +1,34 @@
 package coin_change
 
+const inf = 1000
+
 // CoinChange returns the minimum number of coins needed to make amount, or -1.
 func CoinChange(coins []int, amount int) int {
-	return -1
+
+	if amount == 0 {
+		return 0
+	}
+
+	dpArray := []int{}
+
+	for i := 0; i <= amount; i++ {
+		minPossible := inf
+		for _, coin := range coins {
+			if i-coin == 0 {
+				minPossible = 1
+				break
+			}
+
+			if i-coin > 0 && dpArray[i-coin]+1 < minPossible {
+				minPossible = dpArray[i-coin] + 1
+			}
+		}
+		dpArray = append(dpArray, minPossible)
+	}
+
+	finalAmount := dpArray[amount]
+	if finalAmount == inf {
+		return -1
+	}
+	return finalAmount
 }
