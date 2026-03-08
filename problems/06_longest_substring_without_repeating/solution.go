5101048c427c5acbd6df1771ed529bca6ea80507
brensch <brendolini@protonmail.com>
2026-03-07 17:17:46 -0800

    longest substring was hard

--- problems/06_longest_substring_without_repeating/solution.go
+++ problems/06_longest_substring_without_repeating/solution.go
@@ -2,5 +2,35 @@ package longest_substring_without_repeating
 
 // LengthOfLongestSubstring returns the size of the longest substring with unique characters.
 func LengthOfLongestSubstring(s string) int {
-	return 0
+
+	start := 0
+	longest := 0
+
+	for {
+		if start == len(s)-1 {
+			return longest
+		}
+
+		seenChars := []byte{}
+		for i := 0; i+start < len(s); i++ {
+			currentLetter := s[start+i]
+			foundRepeat := false
+			for _, seenChar := range seenChars {
+				if seenChar == currentLetter {
+					foundRepeat = true
+					break
+				}
+			}
+			seenChars = append(seenChars, currentLetter)
+			if foundRepeat {
+				if i > longest {
+					longest = i
+				}
+				start = start + i
+				break
+			}
+		}
+
+	}
+
 }
